093cda2cd6da9312d408a6a8fcdb4f88de3cef3b
rohith.s <rohith.sorim@gmail.com>
2026-09-23 17:50:27 +0530

    cors

--- src/main/java/com/sorim/fleetmanagement/config/CorsConfig.java
+++ src/main/java/com/sorim/fleetmanagement/config/CorsConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "${FRONTEND_URL:http://localhost:5173}")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173", "${FRONTEND_URL:http://localhost:5173}")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
     }
 }
